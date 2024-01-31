621d7be2c0876c05e4522a9e40798ab94d8b2b63
TianranBaoUCSD <tibao@ucsd.edu>
2024-01-31 12:43:25 -0800

    Fixed methods in ArrayExamples and added more tests in ArrayTests
    lab3 progress

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,28 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
-	}
+  }
+
+  @Test
+  public void testReverseInPlace2(){
+    int[] input2 = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input2);
+  }
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input2 = {4,3,2,1};
+    assertArrayEquals(new int[]{1,2,3,4}, ArrayExamples.reversed(input2));
   }
 }
